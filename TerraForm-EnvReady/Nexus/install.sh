cf3a5f454d9218428c96830cb5cf2b575fa46f36
azadmulani3242 <azadmulani3242@gmail.com>
2023-12-31 19:31:21 +0530

    first commit

---
+++ TerraForm-EnvReady/Nexus/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt update -y
+wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc
+echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
+#install docker
+sudo apt-get update
+sudo apt-get install docker.io -y
+sudo usermod -aG docker ubuntu  
+newgrp docker
+sudo chmod 777 /var/run/docker.sock
+docker pull dockette/nexus
+docker run -it --rm  -d -p 8081:8081 dockette/nexus:latest
